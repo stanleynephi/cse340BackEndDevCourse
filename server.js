62225b31ea71c14ea15aa0b2f15a8d267b8255c1
stanleynephi <nul23001@byui.edu>
2025-05-06 20:20:13 +0000

    week01 first deploy after the creation of the index.ejs file and also the need file as well as updates to the server.js file.

--- server.js
+++ server.js
@@ -6,14 +6,37 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expresslayout = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+
+/******************
+ * View Engine and Template
+ */
+app.set("view engine", "ejs")
+app.use(expresslayout)
+app.set("layout", "layouts/layout")
+
+
+
 /* ***********************
- * Routes
+ * Routes is to help the application find all the static files needed for the application
+ * The static files are in the public folder and subfolders and is imported into the server.js file using the static variable.
  *************************/
 app.use(static)
+
+/**route to the index page of the application 
+ * app.get is used to get the request from the client and then send the response back by rendering the index.ejs file.
+*/
+app.get('/', (req,res)=> {
+  res.render('index', {title: "Home"})
+})
+
+
+
 
 /* ***********************
  * Local Server Information
